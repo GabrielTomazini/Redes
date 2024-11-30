a331d98146129fc05c11a7904d5f9f52b9e5739d
Gabriel Tomazini Marani <gabrieltm.ifms@gmail.com>
2024-11-30 19:58:55 -0300

    Pedro's Version

--- clerigo.py
+++ clerigo.py
@@ -4,9 +4,27 @@ class Clerigo:
         self.nome = nome
         self.CA = None
         self.HP = 100
+        self.dano_fisico = 8
+        self.dano_magico = 15
+    
+    def ataqueAcerto(self):
+        tipo_ataque = random.choice(['AD', 'AM'])  # Alterna entre mágico e físico
+        d20 = random.randint(1, 20)
+        if tipo_ataque == 'AM':  # Ataque Físico
+            dano = random.randint(1, self.dano_fisico)
+            return f"{tipo_ataque}{d20:02d}{dano:02d}"
+        else:  # Ataque Mágico
+            dano = random.randint(1, self.dano_magico)
+            return f"{tipo_ataque}{d20:02d}{dano:02d}"
 
-    def ataque(self ,personagemInimigo):
-        return 10
-    def setVida(self, vida):
-        self.HP = vida
+    def especial(self):
+        cura = random.randint(1, 20) + 3
+        self.HP = self.HP + cura
+        print(f"{self.__class__.__name__} usou CURA e recuperou {cura} pontos de vida! Vida atual: {self.HP}")
 
+    
+    def getVida(self):
+        return self.HP
+    
+    def setVida(self, novaVida):
+        self.HP = novaVida
--- servidor.py
+++ servidor.py
@@ -6,9 +6,8 @@ from mago import Mago
 from ladino import Ladino
 from clerigo import Clerigo
 
-# comentario teste versionamento
+# Comentário de versão, testes etc.
 bonusProeficiencia = 3
-
 
 class Personagem:
     def __init__(self, nome, classe):
@@ -24,16 +23,24 @@ class Personagem:
             self.classe = Ladino()
         elif classe == "Clerigo":
             self.classe = Clerigo()
+        else:
+            print("Classe não reconhecida!")
 
     def ataqueRecebido(self, msg):
-
+        """
+        Processa o ataque recebido, considerando se o inimigo acertou ou não
+        e calculando o dano.
+        """
         if msg[:2] == "AD" and int(msg[2:4]) > self.getTeste(msg[4:7]):
             dano = int(msg[7:9])
         elif msg[:2] == "AM" and int(msg[2:4]) > self.classe.CA:
             dano = int(msg[4:6])
+        elif msg[:2] == "ES":  # Verificando se é um ataque especial
+            print(f"O inimigo usou uma habilidade especial!")
         else:
             print("Seu inimigo errou o ATAQUE, seu D20 foi: ", msg[2:4])
             dano = 0
+
         print("DANO RECEBIDO : ", dano)
         nova_vida = self.getVida() - dano
         self.setVida(nova_vida)
@@ -45,16 +52,24 @@ class Personagem:
     def setVida(self, novaVida):
         self.classe.HP = novaVida
 
+    def especial(self):
+        # Chama o especial da classe
+        return self.classe.usarEspecial()
+
+    def getTeste(self, habilidade):
+        return random.randint(1, 20) 
 
 def main():
-
+    # Configurando o servidor
     socketConexao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     endereco = ("127.0.0.1", 50000)
     socketConexao.bind(endereco)
     socketConexao.listen(1)
 
+    # Espera por um cliente (Jogador)
     [jogador, _] = socketConexao.accept()
 
+    # Criando o personagem do servidor
     personagem1 = Personagem(
         input("Digite o nome do Personagem 1: "),
         input("Digite a classe do Personagem 1: "),
@@ -65,34 +80,41 @@ def main():
     while not encerrado:
         print("Sua vez")
 
-        msg = personagem1.classe.ataqueAcerto()  # Atacando o Inimigo,
+        # Aqui o servidor escolhe entre ataque normal ou especial
+        escolha = input("Digite 'A' para atacar ou 'E' para usar especial: ").strip().upper()
+        if escolha == 'A':
+            msg = personagem2.classe.ataqueAcerto()  # Atacar inimigo
+            jogador.send(msg.encode())  # Envia a mensagem de ataque
+        elif escolha == 'E':
+            # Usando o especial da classe
+            msg_especial = personagem2.especial()
+            jogador.send(msg_especial.encode())  # Envia a mensagem do especial
+            print(f"{personagem2.nome} usou seu especial!")
 
-        jogador.send(
-            msg.encode()
-        )  # enviar teste ou D20 e o dano, msg = 'AD' + '18' + '30'
+        jogador.send(msg.encode())  # Envia a mensagem de ataque (ex: 'AD18 30' ou 'ES15 30')
 
-        retorno = jogador.recv(1)  # mensagem
-
+        retorno = jogador.recv(1)  # Recebe o retorno (se for 'D' ou 'V')
         if not retorno:
             sys.exit(-1)
+
         retorno = retorno.decode()
 
         if retorno == "D":
-            acaoInimigo = jogador.recv(9)  # teste ou D20 e o Dano
+            acaoInimigo = jogador.recv(9)  # Recebe o ataque do inimigo
             acaoInimigo = acaoInimigo.decode()
 
-            hpRestante = personagem1.ataqueRecebido(acaoInimigo)
+            hpRestante = personagem1.ataqueRecebido(acaoInimigo)  # Calcula o dano
             print("Seu HP restante: ", hpRestante)
+
             if hpRestante <= 0:
-                jogador.send("V".encode())
+                jogador.send("V".encode())  # Envia 'V' para indicar que o inimigo venceu
                 encerrado = True
                 break
-            # msg = 'AD' + '18' + '30'
+
         elif retorno == "V":
             print(f"{personagem1.nome} venceu!")
             encerrado = True
             break
-
 
 if __name__ == "__main__":
     main()
